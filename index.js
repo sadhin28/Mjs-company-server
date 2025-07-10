7f8bf5c8182da9886ade52d2d701ed85bbcab28a
sadhin28 <taosifsadhin113@gmail.com>
2025-07-11 00:45:57 +0600

    add limit 50 mb

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port =process.env.PORT || 5000;
  const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
 //middlwire
 app.use(cors());
-app.use(express.json())
+app.use(express.json({limit:"50mb"}))
 
 app.get('/',(req,res)=>{
     res.send('Mjs Company Server is running')
