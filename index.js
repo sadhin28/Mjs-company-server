f5be8fcf762d0482c354829b67bf54f36abccfd9
sadhin28 <taosifsadhin113@gmail.com>
2025-07-14 00:04:06 +0600

    update

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const cors = require('cors');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000 
- const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
 //middlwire
 app.use(cors());
 app.use(express.json({limit:"50mb"}))
+
+ const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
 
 app.get('/',(req,res)=>{
     res.send('Mjs Company Server is running')
